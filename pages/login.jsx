7f9f3e29ea4d390eb672f764c71f9be83abff003
bennfsx <bennfsx@gmail.com>
2024-05-05 00:26:06 +0800

    loginpage

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
-const login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+export default function Login() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", padding: 20 }}>
+      <Text style={{ fontSize: 24, marginBottom: 20, color: "orange" }}>
+        scansavvy technology
+      </Text>
+      <Text style={{ fontSize: 18, marginBottom: 10 }}>
+        Sign in to your account
+      </Text>
 
-export default login;
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          marginBottom: 10,
+        }}
+        placeholder="Your Email"
+      />
+
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          marginBottom: 20,
+        }}
+        placeholder="Password"
+        secureTextEntry={true}
+      />
+
+      <TouchableOpacity
+        style={{ backgroundColor: "orange", padding: 10, alignItems: "center" }}
+      >
+        <Text style={{ color: "white" }}>Sign in</Text>
+      </TouchableOpacity>
+
+      <Text style={{ marginTop: 20, color: "blue" }}>Forgot Password?</Text>
+
+      <Text style={{ marginTop: 10 }}>
+        Don't have an account yet?
+        <Text style={{ color: "blue" }}> Sign up</Text>
+      </Text>
+    </View>
+  );
+}
