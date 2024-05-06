e050f32498ea8a56bb12ba823150661f4b2861a1
cheryl04 <cherylthy00@gmail.com>
2024-05-06 23:23:00 +0800

    Update App.jsx

--- ScanSavvyFE/App.jsx
+++ ScanSavvyFE/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Home from "./pages/Home";
 import QRscanner from "./partials/QRscanner";
 
