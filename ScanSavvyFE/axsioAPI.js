88b688aa2d5ff1e95108f9d410a5a23ca260fdda
bennfsx <bennfsx@gmail.com>
2024-05-11 16:05:15 +0800

    local

--- ScanSavvyFE/axsioAPI.js
+++ ScanSavvyFE/axsioAPI.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Create an instance of axios with the API base URL
 const axiosAPI = axios.create({
-  baseURL: "http://cicadahome.tplinkdns.com:6001",
-  //baseURL: "http://127.0.0.1:6001",
+  // baseURL: "http://cicadahome.tplinkdns.com:6001",
+  baseURL: "http://127.0.0.1:6001",
 });
 
 export default axiosAPI;
